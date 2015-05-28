192b88035fced6e52913cde1d711dacb6956e705
Shaun <shaunsnyders@gmail.com>
2015-05-28 18:18:55 +0200

    Project

--- Project/wiki/urls.py
+++ Project/wiki/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
 	url(r'^Categories/',views.categories, name='categories'),
 	url(r'^Category/?(?P<test>\w?/?)/',views.category, name='category'),
 	url(r'^Article/?(?P<test>\w?/?)/',views.article, name='article'),
-	url(r'^123/?(?P<test>\w?/?)/',views.edit_article, name='edit_article'),
+	url(r'^123/?(?P<test>\w?/?)/123',views.edit_article_introduction, name='edit_article_introduction'),
+	url(r'^1231/?(?P<test>\w?/?)123/',views.edit_article_rating, name='edit_article_rating'),
+	url(r'^12323/?(?P<test>\w?/?)123/',views.edit_article_soil, name='edit_article_soil'),
+	url(r'^12334/?(?P<test>\w?/?)123/',views.edit_article_sunlight, name='edit_article_sunlight'),
+	url(r'^12345/?(?P<test>\w?/?)123/',views.edit_article_watering, name='edit_article_watering'),
+
 	#url(r'^',views.category, name='category'),
 	#url(r'^',views.article, name='article'),
 	
--- Project/wiki/views.py
+++ Project/wiki/views.py
@@ -8,6 +8,7 @@ from wiki.models import Article
 
 
 def wiki(request):
+
 	return render(request, 'wiki/wiki.html')
 
 def categories(request,):
@@ -16,6 +17,7 @@ def categories(request,):
 	return render(request, 'wiki/categories.html',context)
 
 def category(request, test):
+	print(test)
 	articles = Article.objects.filter(category_id=test)
 	context = {'articles': articles}
 	return render(request, 'wiki/category.html', context)
@@ -26,12 +28,55 @@ def article(request, test):
 	return render(request, 'wiki/article.html',context)
 
 
-def edit_article(request ,test):
-	selected_choice = request.POST.get('new')
+def edit_article_introduction(request ,test):
+	selected_choice = request.POST.get('intro')
+	thisArticle = Article.objects.get(id=test)
+	thisArticle.article_introduction =selected_choice
+	thisArticle.save()
+
+	thisArticle=Article.objects.get(id=test)
+	context = {'thisArticle': thisArticle}
+	return render(request, 'wiki/article.html',context)
+
+
+
+def edit_article_rating(request ,test):
+	print("Rating")
+	selected_choice = request.POST.get('rat')
 	articleToEdit = Article.objects.get(id=test)
-	articleToEdit.article_introduction =selected_choice
+	articleToEdit.article_rating =selected_choice
+	articleToEdit.save()
+	
+	thisArticle=Article.objects.get(id=test)
+	context = {'thisArticle': thisArticle}
+	return render(request, 'wiki/article.html',context)
+
+def edit_article_soil(request ,test):
+	selected_choice = request.POST.get('sol')
+	articleToEdit = Article.objects.get(id=test)
+	articleToEdit.article_soil =selected_choice
 	articleToEdit.save()
 
-	articleToEdit=Article.objects.get(id=test)
-	context = {'articleToEdit': articleToEdit}
-	return HttpResponse("Edit Successful")
+	thisArticle=Article.objects.get(id=test)
+	context = {'thisArticle': thisArticle}
+	return render(request, 'wiki/article.html',context)
+
+def edit_article_sunlight(request ,test):
+	selected_choice = request.POST.get('sun')
+	articleToEdit = Article.objects.get(id=test)
+	articleToEdit.article_sunlight =selected_choice
+	articleToEdit.save()
+
+	thisArticle=Article.objects.get(id=test)
+	context = {'thisArticle': thisArticle}
+	return render(request, 'wiki/article.html',context)
+
+def edit_article_watering(request ,test):
+	selected_choice = request.POST.get('wat')
+	articleToEdit = Article.objects.get(id=test)
+	articleToEdit.article_watering =selected_choice
+	articleToEdit.save()
+
+	thisArticle=Article.objects.get(id=test)
+	context = {'thisArticle': thisArticle}
+	return render(request, 'wiki/article.html',context)
